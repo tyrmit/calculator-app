fd0b79ea12acde807d5b4d708f2f5284988545ce
tyrmi <tyrmit@hotmail.com>
2022-02-26 17:37:32 +1100

    Styled the main section to take up the full width and height of the view port, and using state to track the selected theme. Main background colour based on theme is now functioning.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import CalcHeader from './components/CalcHeader/CalcHeader';
 import CalcKeypad from './components/CalcKeypad/CalcKeypad';
 import CalcScreen from './components/CalcScreen/CalcScreen';
 
+import classes from './App.module.css';
+import { useState } from 'react';
+
 function App() {
+    const [themeID, setThemeID] = useState(1);
+    const handleThemeUpdate = (newThemeID) => {
+        setThemeID(newThemeID);
+    };
+
     return (
-        <React.Fragment>
-            <CalcHeader></CalcHeader>
-            <main>
-                <CalcScreen></CalcScreen>
-                <CalcKeypad></CalcKeypad>
-            </main>
-        </React.Fragment>
+        <main className={[classes.App, classes[`Theme${themeID}`]].join(' ')}>
+            <CalcHeader
+                theme={themeID}
+                themeUpdate={handleThemeUpdate}
+            ></CalcHeader>
+            <CalcScreen theme={themeID}></CalcScreen>
+            <CalcKeypad theme={themeID}></CalcKeypad>
+        </main>
     );
 }
 
